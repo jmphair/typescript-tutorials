f99ee765978e2b2c2bd6d5f88be63e96e3b26dfe
Jonathan Phair <jmphair@gmail.com>
2023-04-11 15:52:39 -0400

    edit: remove uneeded files

--- 2-functions-test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var _2_functions_1 = require("./2-functions");
-console.log((0, _2_functions_1.default)(1, 2));
-// console.log(addNumbers(1, "jack"));
-console.log((0, _2_functions_1.addStrings)("a", "b"));
-console.log((0, _2_functions_1.addStrings)("a"));
-console.log((0, _2_functions_1.getName)({ first: "Jack", last: "Herrington" }));
